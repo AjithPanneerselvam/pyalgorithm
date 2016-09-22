bf3abc737b9862b48053d7d5c5818bd0182d0ea5
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2016-09-22 00:17:05 -0600

    rename

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='algo',
+    name='pyalgorithm',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -29,7 +29,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/AjithPanneerselvam',
+    url='https://github.com/AjithPanneerselvam/algo.git',
 
     # Author details
     author='Ajith Panneerselvam',
