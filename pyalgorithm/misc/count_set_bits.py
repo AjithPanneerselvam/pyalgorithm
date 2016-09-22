f1db15b521110d3c9243e72963af7a4cb70243ab
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2016-09-22 01:23:36 -0600

    setup

--- pyalgorithm/misc/count_set_bits.py
+++ pyalgorithm/misc/count_set_bits.py
@@ -4,12 +4,12 @@ Time Complexity - O(log n)
 Key idea: n & (n-1)
 """
 
-def brian_kerninghan(n):
+def count(n):
     count = 0
     while (n != 0):
         n = n & (n-1)
         count += 1
     return count
 
-n = int(raw_input("Enter any interger \n"))
-print brian_kerninghan(n)
+#n = int(raw_input("Enter any interger \n"))
+#print brian_kerninghan(n)
