7eadb1f3cb20bd748ae304a7898e51b56e3b6209
Ajith Panneerselvam <ajithpanneerselvam20@gmail.com>
2017-03-19 05:00:52 -0600

    Error fixed.
    Thanks dani!

--- pyalgorithm/misc/two_power_n.py
+++ pyalgorithm/misc/two_power_n.py
@@ -4,7 +4,7 @@ To check whether a number is divisible by powers of 2
 
 def find(number):
     if number == 1:
-        print True
+        return True
 
     elif(not(number and number-1)):
         return True
