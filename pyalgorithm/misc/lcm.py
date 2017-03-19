2d12e95dacdff08873ebaee1da3247f4efe848f0
christopher daniel <christopherdaniel13397@gmail.com>
2017-03-19 16:37:55 +0530

    improving the algorithm effiency
    temp_a variable can have max(a,b) and in loop can use min(a,b) to reduce the number of iterations.

--- pyalgorithm/misc/lcm.py
+++ pyalgorithm/misc/lcm.py
@@ -4,7 +4,8 @@
 
 
 def lcm(a, b):
-    tmp_a = a
-    while (tmp_a % b) != 0:
-        tmp_a += a
+    tmp = tmp_a = max(a,b)
+    tmp_b = min(a,b)
+    while (tmp_a % tmp_b) != 0:
+        tmp_a += tmp
     return tmp_a
